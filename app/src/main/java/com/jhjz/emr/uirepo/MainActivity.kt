6f456d56e6808ef3ff9a703b02545b0fa5f52cb8
zwq <1031900805@qq.com>
2020-08-21 16:44:56 +0800

    Initial commit

---
+++ app/src/main/java/com/jhjz/emr/uirepo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jhjz.emr.uirepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
